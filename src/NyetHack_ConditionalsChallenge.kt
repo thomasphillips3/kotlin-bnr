0cd86f2c6d0dc7b1313f467d6f1d587b7623238f
Thomas Phillips <tphillips@snap.com>
2019-02-05 15:15:24 -0800

    Refactor to functions

--- src/NyetHack_ConditionalsChallenge.kt
+++ src/NyetHack_ConditionalsChallenge.kt
@@ -1,22 +1,22 @@
 fun main(args: Array<String>) {
     val name = "Madrigal"
-    var healthPoints = 89
+    val healthPoints = (0..100).random()
     var isBlessed = true
     val isImmortal = false
-    val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20 ) .toInt()
 
     // Aura
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = when (karma) {
-        in 0..5 -> "RED"
-        in 6..10 -> "ORANGE"
-        in 11..15 -> "PURPLE"
-        in 16..20 -> "GREEN"
-        else -> "NONE"
-    }
+    val auraColor = auraColor(healthPoints)
 
     // Health
-    val healthStatus = when (healthPoints) {
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+    // Status
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus, healthPoints, isImmortal)
+
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
+    return when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
         in 75..89 -> if (isBlessed) {
@@ -25,11 +25,32 @@ fun main(args: Array<String>) {
             "has some minor wounds."
         }
         in 15..74 -> "looks pretty badly hurt."
-        else -> "knocking on death's door"
+        else -> "is knocking on death's door"
     }
+}
 
-    // Player status
-    println("Aura: ${if (auraVisible) "$auraColor" else "NO AURA" }" +
-        "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
+private fun printPlayerStatus(auraColor: String, isBlessed: Boolean, name: String, healthStatus: String, healthPoints: Int, isImmortal: Boolean) {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+
+    println("Health points: $healthPoints")
+
+    if (auraVisible) {
+        println("Aura: $auraColor " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+        println("$name $healthStatus")
+
+    } else
+        println("NO AURA")
+
+}
+
+private fun auraColor(healthPoints: Int): String {
+    val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20 ) .toInt()
+
+    return when (karma) {
+        in 0..5 -> "RED"
+        in 6..10 -> "ORANGE"
+        in 11..15 -> "PURPLE"
+        in 16..20 -> "GREEN"
+        else -> "NONE"
+    }
 }

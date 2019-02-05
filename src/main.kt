025e3790b5b20bceedee18544d0d4ec5fc5ad7a0
Thomas Phillips <tphillips@snap.com>
2019-02-05 11:43:29 -0800

    Chapter 2 challenges

---
+++ src/main.kt
@@ -0,0 +1,10 @@
+var hasSteed: Boolean = false
+var pubName = "The Unicorn\'s Horn"
+var playerName = "Estragon"
+var publican: String = ""
+var gold: Double = 0.0
+val menu = emptyMap<String, Double>()
+
+fun main(args : Array<String>) {
+    println(playerName.reversed())
+}

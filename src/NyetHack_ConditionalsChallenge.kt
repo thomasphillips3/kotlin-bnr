6d7a381c844a4449b0359c65260b5b474bdd0583
Thomas Phillips <tphillips@snap.com>
2019-02-05 16:59:13 -0800

    Chapter 4 challenges

--- src/NyetHack_ConditionalsChallenge.kt
+++ src/NyetHack_ConditionalsChallenge.kt
@@ -13,20 +13,21 @@ fun main(args: Array<String>) {
     // Status
     printPlayerStatus(auraColor, isBlessed, name, healthStatus, healthPoints, isImmortal)
 
+    // Fireball
+    println("Inebriation Status: ${inebriationStatus(castFireball((0..10).random()))}")
+
 }
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
-    return when (healthPoints) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) {
-            "has some minor wounds but is healing quickly."
-        } else {
-            "has some minor wounds."
-        }
-        in 15..74 -> "looks pretty badly hurt."
-        else -> "is knocking on death's door"
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) = when (healthPoints) {
+    100 -> "is in excellent condition!"
+    in 90..99 -> "has a few scratches."
+    in 75..89 -> if (isBlessed) {
+        "has some minor wounds but is healing quickly."
+    } else {
+        "has some minor wounds."
     }
+    in 15..74 -> "looks pretty badly hurt."
+    else -> "is knocking on death's door"
 }
 
 private fun printPlayerStatus(auraColor: String, isBlessed: Boolean, name: String, healthStatus: String, healthPoints: Int, isImmortal: Boolean) {
@@ -52,5 +53,27 @@ private fun auraColor(healthPoints: Int): String {
         in 11..15 -> "PURPLE"
         in 16..20 -> "GREEN"
         else -> "NONE"
+    }
+}
+
+private fun castFireball(numBalls: Int = 2): Int {
+    println("$numBalls shot ${if (numBalls > 1) "glasses" else "glass"}" + " of Fireball appeared.")
+    return inebriationLevel(numBalls)
+
+}
+
+private fun inebriationLevel(numBalls: Int) : Int {
+    return numBalls * 5
+
+}
+
+private fun inebriationStatus(drunkenness: Int) : String {
+    return when(drunkenness) {
+        in 1..10 -> "tipsy"
+        in 11..20 -> "good"
+        in 21..30 -> "lit"
+        in 31..40 -> "gone"
+        in 41..50 -> "wgw"
+        else -> "whoa"
     }
 }
